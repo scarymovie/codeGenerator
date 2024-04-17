c0f8a546fb292e54bf6314e19982c165b3fb68ae
evgeniy.vinokurov <evgeniy.vinokurov@yoso.ru>
2024-04-17 18:35:11 +0300

    tmp version

--- processFileController.go
+++ processFileController.go
@@ -8,7 +8,7 @@ import (
 )
 
 func processFileController(path string, directory string, actionParams []string, openAPI OpenAPI) {
-	tmpl, err := template.ParseFiles("templateController.go")
+	tmpl, err := template.ParseFiles("templateController.txt")
 	if err != nil {
 		fmt.Printf("Error loading template: %s\n", err)
 		return
@@ -49,11 +49,16 @@ func generateControllerFile(tmpl *template.Template, yamlName string, actionPara
 		}
 	}
 
+	actionParamsDeclaration := ""
+	for _, param := range actionParams {
+		actionParamsDeclaration += fmt.Sprintf("${%s} = $request->get('%s');\n\t", param, param)
+	}
+
 	err = tmpl.Execute(file, map[string]string{
 		"Module":       strings.Title(yamlName),
 		"Operation":    strings.Title(operationId),
 		"actionVar":    strings.ToLower(string(operationId[0])) + operationId[1:],
-		"actionParams": strings.Join(actionParams, ","),
+		"actionParams": actionParamsDeclaration,
 	})
 
 	if err != nil {
